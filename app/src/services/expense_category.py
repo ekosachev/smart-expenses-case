1d542c0f77e649b46e327a839eedde5ce1e0e4d2
kap1anittt <kap1anittt@gmail.com>
2025-06-13 14:34:09 +0300

    New branch

--- app/src/services/expense_category.py
+++
@@ -1,7 +0,0 @@
-from .general_service import BaseService
-from ..database.expense_category import ExpenseCategoryRepository
-
-
-class ExpenseCategoryService(BaseService):
-    def __init__(self):
-        super().__init__(ExpenseCategoryRepository())
