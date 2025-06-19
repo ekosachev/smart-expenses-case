e646927cf5ce3570b0e5a3603b1fb165374bcc25
kap1anittt <kap1anittt@gmail.com>
2025-06-19 14:57:40 +0300

    оп оп

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import json
 from model import RAG_model
+import os
 
 def main():
     # Загружаем данные из JSON файла
@@ -11,8 +12,14 @@ def main():
     
     # Получаем анализ расходов
     analysis = rag_model.analyze_expenses(statistics)
-    
-    # Сохраняем результат в текстовый файл
+
+    print('API_LLM:', os.getenv('API_LLM'))
+    print('ANALYSIS:', analysis)
+
+    # Сохраняем результат в текстовый файлё
+    #with open('expenses_analysis.txt', 'w', encoding='utf-8') as file:
+        #file.write(analysis)
+
     with open('expenses_analysis.md', 'w', encoding='utf-8') as file:
         file.write(analysis)
 
